6bf4e0353b32936ae35ba5f80bcf278f8de3b7ab
Mark Atherton <mark@insightinternet.co.uk>
2022-02-01 12:20:04 +0000

    added ProductDetail page and placeholder markup into Products

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,14 @@
 const Product = () => {
-  return <h1>The Product Page</h1>;
+  return (
+    <section>
+      <h1>The Product Page</h1>
+      <ul>
+        <li>Product 1</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+      </ul>
+    </section>
+  );
 };
 
 export default Product;
