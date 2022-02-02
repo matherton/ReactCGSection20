c57b2843922855d47bcb8c9346a5118198386567
Mark Atherton <mark@insightinternet.co.uk>
2022-02-02 19:38:01 +0000

    changed Route path of <ProductDetail /> to products and added Link's to <Products />

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,11 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Product = () => {
   return (
     <section>
       <h1>The Product Page</h1>
       <ul>
-        <li>a book</li>
-        <li>a carpet</li>
-        <li>a rug</li>
+        <li>
+          <Link to="/products/p1">a book</Link>
+        </li>
+        <li>
+          <Link to="/products/p2">a carpet</Link>
+        </li>
+        <li>
+          <Link to="/products/p3">a rug</Link>
+        </li>
       </ul>
     </section>
   );
